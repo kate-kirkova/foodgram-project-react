8b20d05a6f0c71d7e9af74e033864b7430429b03
Екатерина <chelish81@yandex.ru>
2022-06-15 16:05:44 +0300

    15.6

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'op)z)(r5qw6xws-msi_q_w=@s2)n&lcu1%0&qw(ad3o^&mcdq$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '[::1]', 'web', '178.154.199.240']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '[::1]', 'web', '178.154.192.217']
 
 
 # Application definition
