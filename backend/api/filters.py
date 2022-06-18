cc4e5d1f0a87dab1df70ab11c8bd17172110e20c
Екатерина <chelish81@yandex.ru>
2022-06-18 15:19:07 +0300

    18.6

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,14 +1,14 @@
-from django_filters import CharFilter
-from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Ingredient, Recipe
+from django_filters import rest_framework as django_filter
+from rest_framework import filters
+from recipes.models import Recipe
 from users.models import User
 
 
-class RecipeFilters(FilterSet):
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+class RecipeFilters(django_filter.FilterSet):
+    author = django_filter.ModelChoiceFilter(queryset=User.objects.all())
+    tags = django_filter.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = django_filter.BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = django_filter.BooleanFilter(
         method='get_is_in_shopping_cart')
 
     class Meta:
@@ -25,10 +25,6 @@ class RecipeFilters(FilterSet):
             return queryset.filter(carts__user=self.request.user)
         return queryset.all()
 
-
-class IngredientSearchFilter(FilterSet):
-    name = CharFilter(field_name='name', method='name_filter')
-
-    class Meta:
-        model = Ingredient
-        fields = ('name',)
+ 
+class IngredientSearchFilter(filters.SearchFilter):
+    search_param = 'name' 
