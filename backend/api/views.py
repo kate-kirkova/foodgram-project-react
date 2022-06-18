358639507addc8d6f51112d48de2c1191cfb152b
Екатерина <chelish81@yandex.ru>
2022-06-18 18:06:55 +0300

    3

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,13 @@
 from http import HTTPStatus
-from rest_framework.decorators import action
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
 
@@ -114,29 +116,41 @@ class FavoriteViewSet(BaseFavoriteCartViewSet):
 class DownloadCart(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=False, methods=('get',),
-            url_path='download_shopping_cart')
-    def download_shopping_cart(self, request):
-        shopping_list = {}
-        ingredients = IngredientRecipe.objects.filter(
-            recipes__carts__user=request.user).values(
-                'ingredient__name',
-                'ingredient__measurement_unit'
-        ).annotate(amount=Sum('amount'))
-        for ingredient in ingredients:
-            amount = ingredient['amount']
-            name = ingredient['ingredient__name']
-            measurement_unit = ingredient['ingredient__measurement_unit']
-            if name not in shopping_list:
-                shopping_list[name] = {
-                    'measurement_unit': measurement_unit,
-                    'amount': amount
-                }
-            else:
-                shopping_list[name]['amount'] += ingredient.amount__sum
-        wishlist = ([f'{item} - {value["amount"]} '
-                     f'{value["measurement_unit"]} \n'
-                     for item, value in shopping_list.items()])
-        response = HttpResponse(wishlist, 'Content-Type: text/plain')
-        response['Content-Disposition'] = 'attachment; filename="shoplist.txt"'
+    @staticmethod
+    def canvas_method(dictionary):
+        response = HttpResponse(content_type='application/pdf')
+        response[
+            'Content-Disposition'] = 'attachment; \
+        filename = "shopping_cart.pdf"'
+        begin_position_x, begin_position_y = 40, 650
+        sheet = canvas.Canvas(response, pagesize=A4)
+        pdfmetrics.registerFont(TTFont('FreeSans',
+                                       'data/FreeSans.ttf'))
+        sheet.setFont('FreeSans', 50)
+        sheet.setTitle('Список покупок')
+        sheet.drawString(begin_position_x,
+                         begin_position_y + 40, 'Список покупок: ')
+        sheet.setFont('FreeSans', 24)
+        for number, item in enumerate(dictionary, start=1):
+            if begin_position_y < 100:
+                begin_position_y = 700
+                sheet.showPage()
+                sheet.setFont('FreeSans', 24)
+            sheet.drawString(
+                begin_position_x,
+                begin_position_y,
+                f'{number}.  {item["ingredient__name"]} - '
+                f'{item["ingredient_total"]}'
+                f' {item["ingredient__measurement_unit"]}'
+            )
+            begin_position_y -= 30
+        sheet.showPage()
+        sheet.save()
         return response
+
+    def download(self, request):
+        result = IngredientRecipe.objects.filter(
+            recipe__carts__user=request.user).values(
+            'ingredient__name', 'ingredient__measurement_unit').order_by(
+                'ingredient__name').annotate(ingredient_total=Sum('amount'))
+        return self.canvas_method(result)
